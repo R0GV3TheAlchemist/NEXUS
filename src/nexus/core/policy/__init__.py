5f2b65ea93c16d45def88f4711562a44600b084b
R0GV3 the Alchemist <xxkylesteenxx@outlook.com>
2026-08-03 18:05:02 -0500

    feat: export is_quarantine_candidate from policy package

--- src/nexus/core/policy/__init__.py
+++ src/nexus/core/policy/__init__.py
@@ -6,6 +6,16 @@ package and should not be used; see DECISIONS.md simulation boundary notes.
 """
 
 from .aaa import decide, account
-from .assessment import assess_ability, should_accept_ability
+from .assessment import (
+    assess_ability,
+    should_accept_ability,
+    is_quarantine_candidate,
+)
 
-__all__ = ["decide", "account", "assess_ability", "should_accept_ability"]
+__all__ = [
+    "decide",
+    "account",
+    "assess_ability",
+    "should_accept_ability",
+    "is_quarantine_candidate",
+]
